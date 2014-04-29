3264f233abfe99cceae98889ab70df9b790af9ac
Peter Hazenberg <peter@exa.nl>
2014-04-30 00:25:42 +0200

    Port tomorrow's javascript to wiring and improve all the things

--- step_into_the_future.ino
+++ step_into_the_future.ino
@@ -1,15 +1,14 @@
-int pin_hall = 2;
-
 volatile int last_millis = 0;
+volatile float current_speed = 0;
+float display_speed = 0;
+int count = 0;
 
 void update_speed() {
   int current_millis = millis();
   float rev_millis = current_millis - last_millis;
   last_millis = current_millis;
-  if(rev_millis > 5 && rev_millis < 5000) {
-    float speed_kph = 1 / rev_millis * 2206;
-    Serial.print(speed_kph);
-    Serial.print(" km/h\n");
+  if(rev_millis > 10 && rev_millis < 1000) {
+    current_speed = 1 / rev_millis * 2206;
   }
 }
 
@@ -20,5 +19,21 @@ void setup() {
 }
 
 void loop() {
-  delay(1000);
+  display_speed += (current_speed - display_speed)/2000;
+
+  long current_millis = millis();
+  long idle_millis = current_millis - last_millis;
+  if(idle_millis > 1000) {
+    current_speed = 0;
+  }
+  
+  // start debug code
+  count++;
+  if(count > 500)
+  {
+    count = 0;
+    Serial.print(display_speed);
+    Serial.print("\n");
+  }
+  // end debug code
 }
