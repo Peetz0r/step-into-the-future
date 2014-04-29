9e01cf1fcf3e2877b15d165c576e4214ef36d4b5
Peter Hazenberg <peter@exa.nl>
2014-04-30 00:40:03 +0200

    chamge milliseconds in microseconds (increasing time resolution), also changing somt ints to longs, and added comments

--- step_into_the_future.ino
+++ step_into_the_future.ino
@@ -1,32 +1,50 @@
-volatile int last_millis = 0;
+volatile long last_micros = 0;
 volatile float current_speed = 0;
+volatile float rev_micros = 0;
 float display_speed = 0;
-int count = 0;
+long count = 0;
 
+// this function is the interrupt executed on each rising edge coming from the hall sensor
 void update_speed() {
-  int current_millis = millis();
-  float rev_millis = current_millis - last_millis;
-  last_millis = current_millis;
-  if(rev_millis > 10 && rev_millis < 1000) {
-    current_speed = 1 / rev_millis * 2206;
+  // get current time
+  long current_micros = micros();
+  // caculate difference
+  rev_micros = current_micros - last_micros;
+  // store current time for next time
+  last_micros = current_micros;
+
+  // sanity check (will fail when the sketch had just started, and also debounces the input)
+  if(rev_micros > 10000 && rev_micros < 1000000) {
+    // magic calulation happens here
+    current_speed = 1 / rev_micros * 2206000;
   }
 }
 
 void setup() {
+  // Serial is only used for debugging
   Serial.begin(115200);
+  // our sensor requires a pullup resistor
   pinMode(2, INPUT_PULLUP);
+  // register the interrupt handle
   attachInterrupt(0, update_speed, RISING);
 }
 
+// the loop function is supposed to smooth the readings
 void loop() {
+  // adjust the display speed a little bit towards the actual value
   display_speed += (current_speed - display_speed)/2000;
 
-  long current_millis = millis();
-  long idle_millis = current_millis - last_millis;
-  if(idle_millis > 1000) {
+  // get the current time
+  long current_micros = micros();
+  // calculate difference
+  long idle_micros = current_micros - last_micros;
+
+  // if the last interrupt is longer ago than 2 revolutions (with a maximum of 2 seconds)
+  if(idle_micros > max(1000, rev_micros*2)) {
+    // assume we are not moving, set current speed to 0
     current_speed = 0;
   }
-  
+
   // start debug code
   count++;
   if(count > 500)
